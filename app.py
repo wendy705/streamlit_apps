0a9c44e867fc4f2fd3a7388f96444660c3688542
Guendalina Caldarini <62213824+wendy705@users.noreply.github.com>
2022-08-23 10:36:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def generate_title():
     }
 
     # compute predictions
-    outputs = model.generate(**inputs, do_sample=True, min_length = text_len, temperature =temperature, early_stopping=True, max_length = 256, num_return_sequences=num_gen)
+    outputs = model.generate(**inputs, do_sample=True, min_length = text_len, temperature =temperature, early_stopping=True, max_length = 256, num_return_sequences=num_titles)
     
     results = ""
     final_outputs = []
@@ -103,9 +103,62 @@ def generate_title():
 #     predicted_titles = [nltk.sent_tokenize(decoded_output.strip())[0] for decoded_output in decoded_outputs]
 
     st.session_state.titles = final_outputs
+    
+def generate_beam_title():
+    st.session_state.text = st_text_area
 
+    # tokenize text
+    inputs = ["paraphrase: " + st_text_area]
+    inputs = tokenizer(inputs, return_tensors="pt")
+    
+
+    # compute span boundaries
+    num_tokens = len(inputs["input_ids"][0])
+    print(f"Input has {num_tokens} tokens")
+    max_input_length = 500
+    text_len = sum([i.strip(string.punctuation).isalpha() for i in st_text_area.split()]) - 15
+    num_spans = math.ceil(num_tokens / max_input_length)
+    print(f"Input has {num_spans} spans")
+    overlap = math.ceil((num_spans * max_input_length - num_tokens) / max(num_spans - 1, 1))
+    spans_boundaries = []
+    start = 0
+    for i in range(num_spans):
+        spans_boundaries.append([start + max_input_length * i, start + max_input_length * (i + 1)])
+        start -= overlap
+    print(f"Span boundaries are {spans_boundaries}")
+    spans_boundaries_selected = []
+    j = 0
+    for _ in range(num_titles):
+        spans_boundaries_selected.append(spans_boundaries[j])
+        j += 1
+        if j == len(spans_boundaries):
+            j = 0
+    print(f"Selected span boundaries are {spans_boundaries_selected}")
+
+    # transform input with spans
+    tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]] for boundary in spans_boundaries_selected]
+    tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]] for boundary in spans_boundaries_selected]
+
+    inputs = {
+        "input_ids": torch.stack(tensor_ids),
+        "attention_mask": torch.stack(tensor_masks)
+    }
+
+    # compute predictions
+    batch = tokenizer(prompt, truncation=True, padding='longest',
+                            return_tensors="pt"
+    prediction = model.generate(**batch, num_beams=num_titles,
+                                      num_return_sequences=n_gens, temperature=temperature)
+    results = tokenizer.batch_decode(prediction, skip_special_tokens=True)
+    
+#     decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    final_outputs = [nltk.sent_tokenize(decoded_output.strip())[0] for decoded_output in decoded_outputs]
+
+    st.session_state.titles = final_outputs
 # generate title button
 st_generate_button = st.button('Generate paraphrase', on_click=generate_title)
+st_generate_button = st.button('Generate with beam search', on_click=generate_beam_title)
+
 
 # title generation labels
 if 'titles' not in st.session_state:
