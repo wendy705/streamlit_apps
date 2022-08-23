eb4c1bb5bb491197dade414896af4d1a3d63ba8b
Guendalina Caldarini <62213824+wendy705@users.noreply.github.com>
2022-08-23 11:18:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st_model_load.text("")
 with st.sidebar:
     st.header("Model parameters")
     if 'num_titles' not in st.session_state:
-        st.session_state.num_titles = 5
+        st.session_state.num_titles = 2
     def on_change_num_titles():
         st.session_state.num_titles = num_titles
     num_titles = st.slider("Number of paraphrases to generate", min_value=1, max_value=10, value=1, step=1, on_change=on_change_num_titles)
@@ -52,42 +52,44 @@ def generate_title():
     # tokenize text
     inputs = ["paraphrase: " + st_text_area]
     inputs = tokenizer(inputs, return_tensors="pt")
+    text_len = sum([i.strip(string.punctuation).isalpha() for i in inputs.split()]) - 15
+
     
 
     # compute span boundaries
-    num_tokens = len(inputs["input_ids"][0])
-    print(f"Input has {num_tokens} tokens")
-    max_input_length = 500
-    text_len = sum([i.strip(string.punctuation).isalpha() for i in st_text_area.split()]) - 15
-    num_spans = math.ceil(num_tokens / max_input_length)
-    print(f"Input has {num_spans} spans")
-    overlap = math.ceil((num_spans * max_input_length - num_tokens) / max(num_spans - 1, 1))
-    spans_boundaries = []
-    start = 0
-    for i in range(num_spans):
-        spans_boundaries.append([start + max_input_length * i, start + max_input_length * (i + 1)])
-        start -= overlap
-    print(f"Span boundaries are {spans_boundaries}")
-    spans_boundaries_selected = []
-    j = 0
-    for _ in range(num_titles):
-        spans_boundaries_selected.append(spans_boundaries[j])
-        j += 1
-        if j == len(spans_boundaries):
-            j = 0
-    print(f"Selected span boundaries are {spans_boundaries_selected}")
+#     num_tokens = len(inputs["input_ids"][0])
+#     print(f"Input has {num_tokens} tokens")
+#     max_input_length = 500
+#     num_spans = math.ceil(num_tokens / max_input_length)
+#     print(f"Input has {num_spans} spans")
+#     overlap = math.ceil((num_spans * max_input_length - num_tokens) / max(num_spans - 1, 1))
+#     spans_boundaries = []
+#     start = 0
+#     for i in range(num_spans):
+#         spans_boundaries.append([start + max_input_length * i, start + max_input_length * (i + 1)])
+#         start -= overlap
+#     print(f"Span boundaries are {spans_boundaries}")
+#     spans_boundaries_selected = []
+#     j = 0
+#     for _ in range(num_titles):
+#         spans_boundaries_selected.append(spans_boundaries[j])
+#         j += 1
+#         if j == len(spans_boundaries):
+#             j = 0
+#     print(f"Selected span boundaries are {spans_boundaries_selected}")
 
-    # transform input with spans
-    tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]] for boundary in spans_boundaries_selected]
-    tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]] for boundary in spans_boundaries_selected]
+#     # transform input with spans
+#     tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]] for boundary in spans_boundaries_selected]
+#     tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]] for boundary in spans_boundaries_selected]
 
-    inputs = {
-        "input_ids": torch.stack(tensor_ids),
-        "attention_mask": torch.stack(tensor_masks)
-    }
+#     inputs = {
+#         "input_ids": torch.stack(tensor_ids),
+#         "attention_mask": torch.stack(tensor_masks)
+#     }
 
     # compute predictions
-    outputs = model.generate(**inputs, do_sample=True, min_length = text_len, temperature =temperature, early_stopping=True, max_length = 256, num_return_sequences=num_titles)
+    input_ids, attention_masks = inputs["input_ids"].to(device), inputs["attention_mask"].to(device)
+    outputs = model.generate(input_ids=input_ids, attention_mask=attention_masks, do_sample=True, min_length = text_len, temperature =temperature, early_stopping=True, max_length = 256, num_return_sequences=num_titles)
     
     results = ""
     final_outputs = []
